5871e156feba4790282cbddc1bb3739406dd84dd
McKenzie <mac.gray.311@gmail.com>
2018-04-14 21:48:54 -0700

    part of last commit

---
+++ Assets/Scripts/PowerupManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerupManager : MonoBehaviour {
+
+	public void increaseFireRate(float amount, int duration) {
+		StartCoroutine (increaseFireRateCoroutine (amount, duration));
+	}
+
+	IEnumerator increaseFireRateCoroutine(float amount, int duration)
+	{
+		this.gameObject.GetComponent<PlayerController> ().secondsBetweenShots -= amount;
+		yield return new WaitForSeconds (duration);
+		this.gameObject.GetComponent<PlayerController> ().secondsBetweenShots += amount;
+	}
+
+	public void increasePlayerSpeed(float amount, int duration) {
+		StartCoroutine (increasePlayerSpeedCoroutine (amount, duration));
+	}
+
+	IEnumerator increasePlayerSpeedCoroutine(float amount, int duration)
+	{
+		this.gameObject.GetComponent<PlayerController> ().speed += amount;
+		yield return new WaitForSeconds (duration);
+		this.gameObject.GetComponent<PlayerController> ().speed -= amount;
+	}
+}
