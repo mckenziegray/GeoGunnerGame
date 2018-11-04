b6c16b4594125221344eb62e97b8b37693d8be88
McKenzie Gray <mac.gray.311@gmail.com>
2018-11-04 14:53:05 -0800

    working on player selector menu

--- Assets/Scripts/HitPoints.cs
+++ Assets/Scripts/HitPoints.cs
@@ -6,9 +6,6 @@ public class HitPoints : MonoBehaviour {
 
 	public int maxHP;
 	private int currentHP;
-	public bool invincible;
-	public int points;
-	public GameObject deathAnimation;
 
 	// Use this for initialization
 	void Start () {
@@ -17,15 +14,10 @@ public class HitPoints : MonoBehaviour {
 	
 	public void Damage (int damage)
 	{
-		if (!invincible) {
-			//Debug.Log (this.gameObject.ToString () + " took " + damage + " damage");
-			this.currentHP -= damage;
-			//Debug.Log (this.gameObject.ToString () + " has " + currentHP + " HP remaining");
-			if (this.currentHP <= 0) {
-				this.currentHP = 0;
-				GameObject.FindWithTag ("GameController").GetComponent<GameController> ().addScore (points);
-				this.Die ();
-			}
+		this.currentHP -= damage;
+		if (this.currentHP <= 0) {
+			this.currentHP = 0;
+			this.Die ();
 		}
 	}
 	public void Heal(int healing)
@@ -37,9 +29,5 @@ public class HitPoints : MonoBehaviour {
 
 	private void Die ()
 	{
-		if (this.deathAnimation != null)
-			Instantiate (deathAnimation, transform.position, transform.rotation);
-		
-		Destroy (this.gameObject);
 	}
 }
