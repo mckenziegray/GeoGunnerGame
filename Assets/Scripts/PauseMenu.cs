0c217abd3e43b911b27e5d76877f4008c3833c51
McKenzie <mac.gray.311@gmail.com>
2018-11-05 09:30:51 -0800

    implemented pause menu; untested

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+	public GameObject pauseMenuPanel;
+	private bool paused;
+
+	// Use this for initialization
+	void Start () {
+		pauseMenuPanel.SetActive (false);
+		paused = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Cancel")) {
+			if (paused)
+				Unpause ();
+			else
+				Pause ();
+		}
+	}
+
+	public void Pause()
+	{
+		Time.timeScale = 0; // Stop time
+
+		pauseMenuPanel.SetActive (true);
+		paused = true;
+	}
+
+	public void Unpause()
+	{
+		pauseMenuPanel.SetActive (false);
+		paused = false;
+
+		Time.timeScale = 1; // Resume time
+	}
+}
