14fd3f7e6a28e51bb34084ccf55fc22d47501bb0
McKenzie <mac.gray.311@gmail.com>
2018-11-05 16:55:36 -0800

    added script for globals

---
+++ Assets/Scripts/Globals.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Globals : MonoBehaviour {
+
+	public static string PP_PLAYER_UNIT = "player";
+	public static string PP_PLAYER_COLOR_RED = "player-r";
+	public static string PP_PLAYER_COLOR_GREEN = "player-g";
+	public static string PP_PLAYER_COLOR_BLUE = "player-b";
+}
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+	public GameObject pauseMenuPanel;
+	private bool paused;
+
+	// Use this for initialization
+	void Start () {
+		pauseMenuPanel.SetActive (false);
+		paused = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Cancel")) {
+			if (paused)
+				Unpause ();
+			else
+				Pause ();
+		}
+	}
+
+	public void Pause()
+	{
+		Time.timeScale = 0; // Freeze time
+		// Any scripts that don't rely on time to work need to be disabled
+		// Input should still work while time is frozen
+		// Why does ESC work while paused but not player input? It works out but it seems suspicious
+
+		pauseMenuPanel.SetActive (true);
+		paused = true;
+	}
+
+	public void Unpause()
+	{
+		pauseMenuPanel.SetActive (false);
+		paused = false;
+
+		Time.timeScale = 1; // Unfreeze time
+	}
+}
